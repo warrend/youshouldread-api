61cda1475ddfc5a438c82c75f124aae0f0c06238
warrend <warrend555@gmail.com>
2017-08-18 16:08:47 -0400

    add book validations

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,3 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id, :title, :author, :pages, :review, :category
+  attributes :id, :title, :author, :pages, :review, :genre, :category
 end
